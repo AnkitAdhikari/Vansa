5f1d4930aa8b56e6a30a0740acdcf8167e9a5f51
AnkitAdhikari <ankitadhikari31512@gmail.com>
2026-01-19 01:51:16 +0545

    edited secrets

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,34 @@
 // Import the functions you need from the SDKs you need
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWS3EF0RjKwuChLHveqh6gtPIP8pSdPXQ",
-  authDomain: "vansa-nepal.firebaseapp.com",
-  projectId: "vansa-nepal",
-  storageBucket: "vansa-nepal.firebasestorage.app",
-  messagingSenderId: "473724967542",
-  appId: "1:473724967542:web:a6804463529c8d97aedd57",
-  measurementId: "G-L7BHBDW3QK",
+  apiKey:
+    Constants.expoConfig?.extra?.firebaseApiKey ||
+    process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain:
+    Constants.expoConfig?.extra?.firebaseAuthDomain ||
+    process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:
+    Constants.expoConfig?.extra?.firebaseProjectId ||
+    process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:
+    Constants.expoConfig?.extra?.firebaseStorageBucket ||
+    process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    Constants.expoConfig?.extra?.firebaseMessagingSenderId ||
+    process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:
+    Constants.expoConfig?.extra?.firebaseAppId ||
+    process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId:
+    Constants.expoConfig?.extra?.firebaseMeasurementId ||
+    process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
